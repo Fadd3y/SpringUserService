8a73fe3febe215b9daf6e1b8d4680d6fb5ae55cb
Faddey Pozdeev <faddey2012@yandex.ru>
2025-06-08 22:21:05 +0300

    Init commit

---
+++ src/main/java/ru/practice/springuserservice/SpringUserServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.practice.springuserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringUserServiceApplication.class, args);
+    }
+
+}
